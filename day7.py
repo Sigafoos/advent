335aec8ca82f85c61c7f7fe305335f9137be61be
Dan Conley <dconley@synacor.com>
2015-12-07 11:38:35 -0500

    I don't particularly like my use of map(), but it works

--- day7.py
+++ day7.py
@@ -2,51 +2,71 @@ import re
 import struct
 import sys
 
-wires = {}
+def resolve(instructions):
+	wires = {}
+	while instructions:
+		line = instructions.pop(0)
+		if line['mod'] is not None:
+			second = line['mod']
+			if line['mod'] in wires:
+				second = wires[line['mod']]
+			elif second.isdigit():
+				 second = int(second)
+			else:
+				instructions.append(line)
+				continue
+
+		if line['input'] is not None:
+			if line['input'].isdigit():
+				inputval = int(line['input'])
+			elif line['input'] in wires:
+				inputval = wires[line['input']]
+			else:
+				instructions.append(line)
+				continue
+
+			if line['op'] is not None:
+				if line['op'] == 'RSHIFT':
+					inputval = inputval >> second
+				elif line['op'] == 'LSHIFT':
+					inputval = inputval << second
+				elif line['op'] == 'OR':
+					inputval = inputval | second
+				elif line['op'] == 'AND':
+					inputval = inputval & second
+				else:
+					print line['op'] , 'is not configured'
+					print wires
+					sys.exit()
+		elif line['op'] == 'NOT':
+			inputval = ~second
+
+		wires[line['out']] = inputval % 65536
+	return wires
+
+def this_sucks(line): # tell me how you really feel
+	global a
+	if line['out'] == 'b':
+		line['input'] = str(a)
+		line['op'] = None
+		line['mod'] = None
+	return line
+
 instructions = []
 
 fp = open('input7.txt', 'r')
 for line in fp:
 	matches = re.match('^((?P<input>\w+) )?((?P<op>[A-Z]+) (?P<mod>\w+) )?-> (?P<out>[a-z]+)', line)
-	instructions.append(matches)
+	instructions.append({
+		'input': matches.group('input'),
+		'op': matches.group('op'),
+		'mod': matches.group('mod'),
+		'out': matches.group('out')
+		})
 
-while instructions:
-	line = instructions.pop(0)
-	if line.group('mod') is not None:
-		second = line.group('mod')
-		if line.group('mod') in wires:
-			second = wires[line.group('mod')]
-		elif second.isdigit():
-			 second = int(second)
-		else:
-			instructions.append(line)
-			continue
-
-	if line.group('input') is not None:
-		if line.group('input').isdigit():
-			inputval = int(line.group('input'))
-		elif line.group('input') in wires:
-			inputval = wires[line.group('input')]
-		else:
-			instructions.append(line)
-			continue
-
-		if line.group('op') is not None:
-			if line.group('op') == 'RSHIFT':
-				inputval = inputval >> second
-			elif line.group('op') == 'LSHIFT':
-				inputval = inputval << second
-			elif line.group('op') == 'OR':
-				inputval = inputval | second
-			elif line.group('op') == 'AND':
-				inputval = inputval & second
-			else:
-				print line.group('op') , 'is not configured'
-				print wires
-				sys.exit()
-	elif line.group('op') == 'NOT':
-		inputval = ~second
-
-	wires[line.group('out')] = inputval % 65536
-
+second = instructions
+wires = resolve(instructions[:])
+print wires['a']
+a = wires['a']
+wires = resolve(map(this_sucks, instructions))
 print wires['a']
