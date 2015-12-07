bb2253def598855295b1b704a97b68c0f3b4ba61
Dan Conley <dconley@synacor.com>
2015-12-07 14:39:34 -0500

    day 2 in python

---
+++ day2.py
@@ -0,0 +1,12 @@
+paper = 0
+ribbon = 0
+
+for line in open('input2.txt', 'r'):
+	dimensions = line.strip().split('x')
+	dimensions = [int(side) for side in dimensions]
+	dimensions.sort()
+	paper += 2 * dimensions[0] * dimensions[1] + 2 * dimensions[1] * dimensions[2] + 2 * dimensions[0] * dimensions[2] + dimensions[0] * dimensions[1]
+	ribbon += 2 * (dimensions[0] + dimensions[1]) + dimensions[0] * dimensions[1] * dimensions[2]
+
+print paper , 'square feet of paper'
+print ribbon , 'feet of ribbon'
