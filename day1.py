fd0a989f400f0b30b87251c207a65bedb12797bd
Dan Conley <dconley@synacor.com>
2015-12-07 13:52:43 -0500

    A Python version of my PHP implementation, which I deleted

---
+++ day1.py
@@ -0,0 +1,14 @@
+floor = 0
+basement = 0
+i = 1
+for line in open('input1.txt', 'r'):
+	for character in line:
+		if (character == '('):
+			floor += 1
+		else:
+			floor -= 1
+			if basement is 0 and floor < 0:
+				basement = i
+		i += 1
+print 'Santa will end on floor' , floor
+print 'Santa first entered the basement on instruction' , basement
