91265261af747c2ef6ebcad3f4d7802f544233e8
Dan Conley <dconley@synacor.com>
2015-12-08 10:43:14 -0500

    Back to PHP

--- php/day8.php
+++ php/day8.php
@@ -1,9 +1,24 @@
 <?php
-$fp = fopen('input8.txt', 'r');
-while ($line = fgets($fp))
+$fp = fopen('../input/input8.txt', 'r');
+$raw = 0;
+$parsed = 0;
+while ($line = trim(fgets($fp)))
 {
-	$string = `php -r 'echo $string;'`;
-	echo $string;
+	$raw += strlen($line);
+	eval('$tmp = ' . $line . ';');
+	$parsed += strlen($tmp);
 }
+echo 'Part 1: ' . ($raw - $parsed) . PHP_EOL;
+
+$raw = 0;
+$parsed = 0;
+rewind($fp);
+while ($line = trim(fgets($fp)))
+{
+	$raw += strlen($line);
+	$parsed += strlen(addcslashes($line, '"\\')) + 2; // you encode the quotes, then add them again?
+}
+echo 'Part 2: ' . ($parsed - $raw) . PHP_EOL;
+
 fclose($fp);
 ?>
