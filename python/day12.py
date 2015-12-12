21651d93d5a07fa72c2fe377d3236e241ff68637
Sigafoos <dan.j.conley@gmail.com>
2015-12-12 10:36:36 -0500

    Day 12 part 2

--- python/day12.py
+++ python/day12.py
@@ -1,17 +1,22 @@
 import json
 
-def add(thing):
+def add(thing, red = True):
 	total = 0
 	if type(thing) is dict:
+		if not red and 'red' in thing.values():
+			return 0
 		for (key, item) in thing.items():
-			total += add(item)
+			if not red and key == 'red':
+				continue
+			total += add(item, red)
 	elif type(thing) is list:
 		for item in thing:
-			total += add(item)
-	elif type(thing) is int or (type(thing) is str and thing.isnumeric()):
+			total += add(item, red)
+	elif type(thing) is int or (type(thing) is str and thing.isdigit()):
 		return int(thing)
 	return total
 
 fp = open('../input/input12.txt')
 json = json.load(fp)
-print add(json)
+print 'Part 1:' , add(json)
+print 'Part 2:' , add(json, False)
