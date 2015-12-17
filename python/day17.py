fc454e1326c034734d3fd96216e655de35c688f6
Dan Conley <dconley@synacor.com>
2015-12-17 15:15:40 -0500

    Still not done :(

--- python/day17.py
+++ python/day17.py
@@ -6,14 +6,23 @@ for line in open('../input/input17.txt'):
 	containers.append(int(line.strip()))
 
 containers = sorted(containers)
+
+# find the max number of containers
 for i in xrange(len(containers)):
 	if sum(containers[:i + 1]) >= 150:
 		break
 combinations = itertools.permutations([key for key in xrange(len(containers))], i)
 
+# find the min number of containers
+for i in xrange(len(containers)):
+	if sum(containers[-(i + 1):]) >= 150:
+		break
+minimum = i
+
 valid = set()
 for combination in combinations:
-	for i in xrange(len(combination)):
+	total = 0
+	for i in xrange(minimum, len(combination)):
 		permutation = [containers[x] for x in combination[:i + 1]]
 		total = sum(permutation)
 		if total > 150:
