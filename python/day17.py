9fda09bfafaa4d19ef16964e333a9137542dca49
Dan Conley <dconley@synacor.com>
2015-12-17 13:46:50 -0500

    Still not very optimized. Like, at all. Saving.

--- python/day17.py
+++ python/day17.py
@@ -1,17 +1,24 @@
 import itertools
+import sys
 
 containers = []
 for line in open('../input/input17.txt'):
 	containers.append(int(line.strip()))
 
-valid = 0
-for combination in itertools.permutations(containers):
-	total = 0
-	for item in combination:
-		total += item
+containers = sorted(containers)
+for i in xrange(len(containers)):
+	if sum(containers[:i + 1]) >= 150:
+		break
+combinations = itertools.permutations([key for key in xrange(len(containers))], i)
+
+valid = set()
+for combination in combinations:
+	for i in xrange(len(combination)):
+		permutation = [containers[x] for x in combination[:i + 1]]
+		total = sum(permutation)
 		if total > 150:
 			break
 		elif total == 150:
-			valid += 1
+			valid.add(tuple(sorted(combination[:i + 1])))
 			break
-print valid
+print len(valid)
