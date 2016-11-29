ca0b1cc7f570b59f54a38dc2716d6e6923c1a95b
Dan Conley <dconley@synacor.com>
2016-11-29 15:42:03 -0500

    Woo, day 20

--- python/day20.py
+++ python/day20.py
@@ -1,48 +1,36 @@
+# yet again I got some algorithm help from the subreddit
 import sys
 
-def divisors(x, two = False):
-	if x < 50 or not two:
-		start = 1
-	else:
-		start = x - 50
-	for i in xrange(1, x/2 + 1): # got this from SO (yeah, I know)
-		if x % i == 0:
-			yield i
-	yield x
-
-def deliver(house, two = False):
-	houses = divisors(house, two)
-	if not two:
-		multiplier = 10
-	else:
-		multiplier = 11
-	return sum(houses) * multiplier
-
 def part1(target):
-	i = 1
-	actual = deliver(i)
-	# this takes basically forever on my server but it worked
-	while actual < target:
-		i += 1
-		if sum([x*10 for x in xrange(i + 1)]) < target:
-			continue # if every elf isn't enough, don't bother
-		actual = deliver(i)
-	return i
+	houses = {}
+	for i in xrange(1, target / 10): # each elf
+		for j in xrange(i, target / 10, i): # each house the elf visits
+			if j not in houses:
+				houses[j] = 0
+			houses[j] += i * 10
+
+	for (number, presents) in houses.iteritems():
+		if presents >= target:
+			return number
 
 def part2(target):
-	i = 1
-	actual = deliver(i, True)
-	# this takes basically forever on my server but it worked
-	while actual < target:
-		i += 1
-		if sum([x*11 for x in xrange(1, ((i + 1) / 2) + 1)]) < target:
-			continue # if every elf isn't enough, don't bother
-		actual = deliver(i, True)
-	return i
+	houses = {}
+	for i in xrange(1, target / 11): # each elf
+		for j in xrange(i, i * 50, i): # each house the elf visits
+			if j > target / 11: # too big! abort!
+				continue
+			if j not in houses:
+				houses[j] = 0
+			houses[j] += i * 11
+
+	for (number, presents) in houses.iteritems():
+		if presents >= target:
+			return number
+
 
 target = int(sys.argv[1])
-#one = part1(target)
-#print 'Part 1:' , one
+one = part1(target)
+print 'Part 1:' , one
 
 two = part2(target)
 print 'Part 2:' , two
