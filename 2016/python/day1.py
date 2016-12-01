e3594b261b30133c0a24c040a82fb794ef3f0567
Dan Conley <dconley@synacor.com>
2016-12-01 10:33:27 -0500

    WOO 2016

---
+++ 2016/python/day1.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+
+def walk():
+	position = [0, 0]
+	facing = 'N'
+	visited = []
+
+	with open('../input/input1.txt') as fp:
+		for line in fp: # only one
+			directions = line.split(', ')
+
+	for direction in directions:
+		turn = direction[0]
+		steps = int(direction[1:])
+		if (facing == 'N' and turn == 'R') or (facing == 'S' and turn == 'L'):
+			for i in xrange(steps):
+				position[0] += 1
+				if position not in visited:
+					visited.append(deepcopy(position))
+				else:
+					return position
+			facing = 'E'
+		elif (facing == 'N' and turn == 'L') or (facing == 'S' and turn == 'R'):
+			for i in xrange(steps):
+				position[0] -= 1
+				if position not in visited:
+					visited.append(deepcopy(position))
+				else:
+					return position
+			facing = 'W'
+		elif (facing == 'E' and turn == 'R') or (facing == 'W' and turn == 'L'):
+			for i in xrange(steps):
+				position[1] -= 1
+				if position not in visited:
+					visited.append(deepcopy(position))
+				else:
+					return position
+			facing = 'S'
+		elif (facing == 'E' and turn == 'L') or (facing == 'W' and turn == 'R'):
+			for i in xrange(steps):
+				position[1] += 1
+				if position not in visited:
+					visited.append(deepcopy(position))
+				else:
+					return position
+			facing = 'N'
+
+	return position
+
+print sum(map(abs, walk()))
