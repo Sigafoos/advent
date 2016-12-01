ca3536d2a02d0844721547a9e016c9429e06339a
Dan Conley <dconley@synacor.com>
2016-12-01 10:34:03 -0500

    2015 24 - did yesterday

--- 2015/python/day24.py
+++ 2015/python/day24.py
@@ -1,30 +1,56 @@
 import itertools
 import sys
-#sleigh = [(0, []), (1, []), (2, [])]
-sleigh = [[0] for x in xrange(3)]
-packages = []
-for line in open('../input/input24.txt'):
-	packages.append(int(line.strip()))
-target = sum(packages) / 3
-packages = sorted(packages, reverse=True)
+from operator import mul
 
-#print 'going for' , target
-#for package in packages:
-#	print [sum(contents) for (section, contents) in sleigh]
-	# sort section by weight asc
-	# if it fits, put in lowest entry
-	# else, uh, this won't work
+def day24(compartments):
+	packages = []
+	for line in open('../input/input24.txt'):
+		packages.append(int(line.strip()))
+	packages.sort(reverse=True)
 
-good = []
-for combination in itertools.permutations(packages):
-	for package in combination:
-		for i in xrange(3):
-			if sum(sleigh[i]) + package <= target:
-				sleigh[i].append(package)
+	target = sum(packages) / compartments
+
+	smallest = []
+	for i in xrange(len(packages) - compartments - 1):
+		for combination in itertools.combinations(packages, i):
+			if sum(combination) == target:
+				smallest.append(reduce(mul, combination))
+
+		if len(smallest):
+			return min(smallest)
+
+# from the reddit megathread this isn't necessary, but I wouldn't know that
+# if I hadn't needed help
+def unused():
+	valid = []
+	for combination in smallest:
+		print 'starting combination'
+		remaining = filter(lambda x: x not in combination, packages) # get what's left
+		for permutation in itertools.permutations(remaining):
+			print 'permutation:'
+			print permutation
+			sleigh = [0, 0]
+			current = 0
+			quantum = 0
+			for package in permutation:
+				if sleigh[current] == target:
+					if current == len(sleigh) - 1: # everything fits
+						print 'adding'
+						quantum = reduce(lambda x, y: x*y, combination)
+						break
+					else:
+						print 'current is now %s' % current
+						current += 1
+				elif sleigh[current] > target: # overloaded
+					print 'sleigh %s is overloaded: NEXT!' % current
+					break
+				sleigh[current] += package
+
+			if quantum > 0:
+				print 'on to the next'
+				valid.append(quantum) # we know this fits perfectly, move on to the next
 				break
-		else:
-			break
-	else:
-		good.append(combination)
+			print 'next permutation'
 
-print good
+print day24(3)
+print day24(4)
