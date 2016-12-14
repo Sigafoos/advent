488ce3c21fdb8d387a7e94ca1215ab2eeae6b1f6
Dan Conley <dan.j.conley@gmail.com>
2016-12-13 21:30:37 -0500

    Look ma, I implemented A*. That was legit really fun.

--- 2016/python/day13.py
+++ 2016/python/day13.py
@@ -1,10 +1,7 @@
 import sys
 
-cosmological_constant = int(sys.argv[1])
-
 def is_wall(x, y):
-	global cosmological_constant
-	return bin((x*x + 3*x + 2*x*y + y + y*y) + cosmological_constant).count('1') % 2 == 1
+	return bin((x*x + 3*x + 2*x*y + y + y*y) + int(sys.argv[1])).count('1') % 2 == 1
 
 def generate_map(size):
 	grid = []
@@ -20,7 +17,7 @@ def draw_map(grid, current, visited, closed, goal):
 	for y in xrange(len(grid)):
 		row = ''
 		for x in xrange(len(grid[y])):
-			row += 'O' if (y, x) == goal else 'X' if (y, x) == current else 'x' if (y, x) in visited else '.' if grid[x][y] == 1 else '#'
+			row += '\033[41mO\033[0m' if (y, x) == goal else '\033[35mX\033[0m' if (y, x) == current else '\033[32mx\033[0m' if (y, x) in visited else '.' if grid[x][y] == 1 else '\033[34m#\033[0m' if grid[x][y] not in closed else '\033[31m=\033[0m'
 		print row
 
 def distance(start, end):
@@ -39,6 +36,7 @@ def pathfind(start, goal, grid):
 	closed_nodes = []
 	visited_nodes = []
 	current = start
+	trace = {}
 	while current != goal:
 		# check in on Yelp to record our visit
 		visited_nodes.append(current)
@@ -46,7 +44,11 @@ def pathfind(start, goal, grid):
 		if current in open_nodes:
 			open_nodes.remove(current)
 
-		open_nodes += filter(lambda node: 0 <= node[0] < len(grid[0]) and 0 <= node[1] < len(grid) and node not in closed_nodes, get_adjacent(current))
+		open_nodes += filter(lambda node: 0 <= node[0] < len(grid[0]) and 0 <= node[1] < len(grid) and node not in closed_nodes and node not in open_nodes, get_adjacent(current))
+
+		for node in get_adjacent(current):
+			if str(node) not in trace:
+				trace[str(node)] = current
 
 		lowest = []
 		lowest_nodes = []
@@ -59,29 +61,42 @@ def pathfind(start, goal, grid):
 				lowest_nodes.append(node)
 
 		if lowest_nodes == []:
+			# didn't come up in the challenge, but if you extended...
 			print 'ack, nowhere to go'
 			draw_map(grid, current, visited_nodes, closed_nodes, goal)
 			sys.exit()
 
 		current = lowest_nodes[0]
 
-	if sorted(visited_nodes) != sorted(open_nodes): # we had branches
-		print 'flip it, reverse it'
-		new_grid = []
-		for y in xrange(len(grid)):
-			row = []
-			for x in xrange(len(grid[y])):
-				price = 999 if (x, y) not in visited_nodes else 1
-				row.append(price)
-			new_grid.append(row)
+	steps = 0
+	while current != start:
+		current = trace[str(current)]
+		steps += 1
+	return steps
 
-		draw_map(new_grid, (), (), (), ())
-		return pathfind(current, start, new_grid)
-	return visited_nodes
-
+def meander(grid, limit):
+	visited = set()
+	to_search = []
+	current = (1, 1)
+	distances = {str(current): 0}
+	while True:
+		visited.add(current)
+		neighbors = filter(lambda node: 0 <= node[0] < len(grid[0]) and 0 <= node[1] < len(grid) and node not in visited and node not in to_search and grid[node[1]][node[0]] == 1, get_adjacent(current))
+		distance = distances[str(current)]
+		for neighbor in neighbors:
+			if distance + 1 <= limit:
+				to_search.append(neighbor)
+				if str(neighbor) in distances and distance + 1 < distances[str(neighbor)]:
+					print 'WE SHOULD REDO THINGS'
+					# this wasn't an issue in the challenge but it could be
+					sys.exit()
+					distances[str(neighbor)] = distance + 1
+				elif str(neighbor) not in distances:
+					distances[str(neighbor)] = distance + 1
+		if len(to_search) == 0:
+			return visited
+		current = to_search.pop(0)
 
 grid = generate_map(50)
-part1 = pathfind((1, 1), (31, 39), grid)
-print 'Part 1: %s' % len(part1)
-
-print draw_map(grid, (31, 39), part1, (), (31, 39))
+print 'Part 1: %s' % pathfind((1, 1), (31, 39), grid)
+print 'Part 2: %s' % len(meander(generate_map(30), 50))
