cbd4621cae1928ce4e7ae25e1a0de31ded7b7428
Dan Conley <dconley@protonmail.com>
2017-12-01 12:15:27 -0500

    2017 bay-bee!

---
+++ 2017/python/day01.py
@@ -0,0 +1,16 @@
+import re
+with open('../inputs/01.txt') as fp:
+        captcha = fp.readline().strip()
+
+pattern = re.compile(r'([\d])(?=\1)')
+matches = pattern.findall(captcha)
+if captcha[0] == captcha[-1]:
+    matches.append(captcha[-1])
+print "Part 1: %s" % sum(map(lambda x: int(x), matches))
+
+matches = []
+length = len(captcha)
+for i in xrange(length):
+    if captcha[i] == captcha[(i + length/2) % length]:
+        matches.append(captcha[i])
+print "Part 2: %s" % sum(map(lambda x: int(x), matches))
