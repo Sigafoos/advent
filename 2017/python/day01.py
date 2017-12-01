9cf9aa531ed8983d4239d6969ac0d13a3845589c
Dan Conley <dconley@protonmail.com>
2017-12-01 12:41:06 -0500

    oh hey I can shrink it a bit more

--- 2017/python/day01.py
+++ 2017/python/day01.py
@@ -6,11 +6,11 @@ pattern = re.compile(r'([\d])(?=\1)')
 matches = pattern.findall(captcha)
 if captcha[0] == captcha[-1]:
     matches.append(captcha[-1])
-print "Part 1: %s" % sum(map(lambda x: int(x), matches))
+print "Part 1: %s" % sum(map(int, matches))
 
 matches = []
 length = len(captcha)
 for i in xrange(length):
     if captcha[i] == captcha[(i + length/2) % length]:
         matches.append(captcha[i])
-print "Part 2: %s" % sum(map(lambda x: int(x), matches))
+print "Part 2: %s" % sum(map(int, matches))
