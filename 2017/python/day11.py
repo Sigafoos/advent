4c12254dbeab808869d6c6d996d536442a9267ff
Dan Conley <dconley@protonmail.com>
2017-12-12 22:44:59 -0500

    whoops found a bug, can have the z axis back now

--- 2017/python/day11.py
+++ 2017/python/day11.py
@@ -3,28 +3,28 @@ with open('../inputs/11.txt') as fp:
 
 # http://keekerdc.com/2011/03/hexagon-grids-coordinate-systems-and-distance-calculations/
 # I searched "hex grid algorithm", yeah
-pos = [0, 0]
+pos = [0, 0, 0]
 farthest = -999
 
 for d in instructions:
 	if d == 'nw':
 		pos[0] -= 1
-		#pos[2] -= 1
+		pos[2] += 1
 	elif d == 'n':
 		pos[0] -= 1
 		pos[1] += 1
 	elif d == 'ne':
 		pos[1] += 1
-		#pos[2] -= 1
+		pos[2] -= 1
 	elif d == 'se':
 		pos[0] += 1
-		#pos[2] -= 1
+		pos[2] -= 1
 	elif d == 's':
 		pos[0] += 1
 		pos[1] -= 1
 	elif d == 'sw':
 		pos[1] -= 1
-		#pos[2] += 1
+		pos[2] += 1
 	else:
 		raise Exception('what the h*ck is %s' % d)
 	distance = max(map(abs, pos))
