3cfe3baba653bbe529c446a1f7e39e15b0b1e447
Dan Conley <dconley@protonmail.com>
2019-12-14 18:24:40 -0500

    finally, part 2

--- 2019/csharp/10/Program.cs
+++ 2019/csharp/10/Program.cs
@@ -29,7 +29,7 @@ namespace _10
                 System.Environment.Exit(1);
             }
 
-            var best = 0;
+            var best = new Asteroid(0, 0);
             foreach (KeyValuePair<string, Asteroid> current in belt) {
                 // "northwest"
                 for (int x = current.Value.X-1; x >= 0; x--) {
@@ -71,24 +71,34 @@ namespace _10
                     }
                 }
 
-                var saw = current.Value.Seen();
-                if (saw > best) {
-                    best = saw;
+                if (current.Value.Seen() > best.Seen()) {
+                    best = current.Value;
                 }
             }
-            Console.WriteLine($"Part 1: {best}");
+            Console.WriteLine($"Part 1: {best.Seen()}");
+
+            best.Sort();
+            while (best.Destroyed.Count < 200 && best.Seen() > 0) {
+                best.Sweep();
+            }
+            var final = best.Destroyed[best.Destroyed.Count-1];
+            Console.WriteLine($"Part 2: {final.X * 100 + final.Y}");
         }
     }
 
     class Asteroid {
         public int X {get;}
         public int Y {get;}
-        protected List<double> tangents;
+        protected Dictionary<double, Queue<Asteroid>> tangents;
+        public List<Asteroid> Destroyed {get; private set;}
+        protected Queue<double> radar;
 
         public Asteroid(int x, int y) {
             this.X = x;
             this.Y = y;
-            this.tangents = new List<double>();
+            this.tangents = new Dictionary<double, Queue<Asteroid>>();
+            this.Destroyed = new List<Asteroid>();
+            this.radar = new Queue<double>();
         }
 
         public void Inspect(Asteroid asteroid) {
@@ -99,8 +109,13 @@ namespace _10
             int y = asteroid.Y - this.Y;
 
             var rads = Math.Atan2(y, x);
-            if (!this.tangents.Contains(rads)) {
-                this.tangents.Add(rads);
+            if (!this.tangents.ContainsKey(rads)) {
+                var queue = new Queue<Asteroid>();
+                queue.Enqueue(asteroid);
+                this.tangents.Add(rads, queue);
+            }
+            else {
+                this.tangents[rads].Enqueue(asteroid);
             }
         }
 
@@ -108,8 +123,49 @@ namespace _10
             return this.tangents.Count;
         }
 
+        public void Sweep() {
+            if (this.radar.Count == 0) {
+                var keys = new double[this.tangents.Count];
+                this.tangents.Keys.CopyTo(keys, 0);
+                Array.Sort(keys);
+                this.radar = new Queue<double>(keys);
+
+                // I don't like this bit
+                while (this.radar.Peek() < Math.Atan2(-1, 0)) {
+                    this.radar.Enqueue(this.radar.Dequeue());
+                }
+            }
+            var tangent = this.radar.Dequeue();
+            this.Destroyed.Add(this.tangents[tangent].Dequeue());
+            if (this.tangents[tangent].Count == 0) {
+                this.tangents.Remove(tangent);
+            }
+        }
+
+        public void Sort() {
+            var tangents = new Dictionary<double, Queue<Asteroid>>();
+            foreach (double key in this.tangents.Keys) {
+                var queue = this.tangents[key].ToArray();
+                Array.Sort(queue, new AsteroidComparer(this));
+                tangents.Add(key, new Queue<Asteroid>(queue));
+            }
+            this.tangents = tangents;
+        }
+
         public override string ToString() {
             return $"{this.X},{this.Y}";
+        }
+    }
+
+    class AsteroidComparer : IComparer<Asteroid> {
+        protected Asteroid asteroid;
+
+        public AsteroidComparer(Asteroid asteroid) {
+            this.asteroid = asteroid;
+        }
+
+        public int Compare(Asteroid a, Asteroid b) {
+            return (Math.Abs(a.X - this.asteroid.X) + Math.Abs(a.Y - this.asteroid.Y)) - (Math.Abs(b.X - this.asteroid.X) + Math.Abs(b.Y - this.asteroid.Y));
         }
     }
 }
