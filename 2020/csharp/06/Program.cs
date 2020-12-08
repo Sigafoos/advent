c88cad0a0fd2cec9f1226bd9bf49046a9381a293
Dan Conley <dconley@acvauctions.com>
2020-12-08 17:23:41 -0500

    d6, b is too low

---
+++ 2020/csharp/06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        class Group
+        {
+            public IEnumerable<char[]> Answers { get; private set; }
+
+            public Group(string responses)
+            {
+                Answers = responses.Split('\n').Select(r => r.ToCharArray());
+            }
+
+            public int CountDistinct()
+            {
+                return Answers
+                    .SelectMany(a => a)
+                    .Distinct()
+                    .Count();
+            }
+
+            public int CountConsensus()
+            {
+                return Answers
+                    .Aggregate(Answers.First(), (current, next) => current.Intersect(next).ToArray())
+                    .Count();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var groups = new List<Group>();
+            string input = File.ReadAllText("../../input/06.txt");
+            foreach (string g in input.Split("\n\n"))
+            {
+                groups.Add(new Group(g));
+            }
+
+            Console.WriteLine($"Part 1: {groups.Select(g => g.CountDistinct()).Sum()}");
+            Console.WriteLine($"Part 2: {groups.Select(g => g.CountConsensus()).Sum()}"); // 3244 too low
+        }
+    }
+}
